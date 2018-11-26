c6c74ad445474f119bedff5486c35a2a0850713a
JesusBetaX <jjbaezmm@yahoo.com>
2018-11-26 15:54:07 -0600

    http client

--- restlight/src/restlight/io/UrlConnectionInputStream.java
+++ restlight/src/restlight/io/UrlConnectionInputStream.java
@@ -1,6 +1,5 @@
 package restlight.io;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
@@ -9,12 +8,13 @@ import java.net.HttpURLConnection;
  * Wrapper for a {@link HttpURLConnection}'s InputStream which disconnects the
  * connection on stream close.
  */
-public class UrlConnectionInputStream extends BufferedInputStream {
+public class UrlConnectionInputStream extends InputStream {
 
+  private final InputStream mInputStream;
   private final HttpURLConnection mConnection;
   
   public UrlConnectionInputStream(HttpURLConnection connection) {
-    super(inputStreamFromConnection(connection));
+    mInputStream = inputStreamFromConnection(connection);
     mConnection = connection;
   }
   
@@ -33,6 +33,10 @@ public class UrlConnectionInputStream extends BufferedInputStream {
     }
   }
 
+  @Override public int read() throws IOException {
+    return mInputStream.read();
+  }
+  
   @Override public void close() throws IOException {
     super.close();
     mConnection.disconnect();
