a0724fa0a3d097feffd6f455e8750ebbcd4ee02c
JesusBetaX <jjbaezmm@yahoo.com>
2019-01-28 10:50:50 -0600

    adapter

--- examples/src/com/jx/gson/Post.java
+++ examples/src/com/jx/gson/Post.java
@@ -2,18 +2,23 @@ package com.jx.gson;
 
 import com.google.gson.annotations.SerializedName;
 import java.util.Date;
+import restlight.body.Field;
 
 public class Post {
   @SerializedName("id")
+  @Field("id")
   public long id;
 
   @SerializedName("date")
+  @Field("date")
   public Date dateCreated;
 
   @SerializedName("title")
+  @Field("title")
   public String title;
   
   @SerializedName("author")
+  @Field("author")
   public String author;
   
   @SerializedName("url")
